a7dbd749185934cb3ef35a6636204eb381a9ff14
Anietie Lawrence Hanson Essien <arrhnnie@gmail.com>
2023-01-01 02:16:13 +0100

    Add files via upload
    Happy New Year! Check out this Calendar.

---
+++ 2023_Calendar.py
@@ -0,0 +1,21 @@
+'''
+2023 Calendar 
+
+Press Run▶️ button
+'''
+
+import calendar
+year = 2023
+month= 1
+for i in range(1,13):
+     print(calendar.month(year,month))
+     month += 1
+
+'''
+ Made By Biggesth3art ©2023
+ 
+_____________________________________________
+ Press Run▶️ button for the output and then Scroll Up ⬆️ to see the full output
+ 
+'''
+ 
